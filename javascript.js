a43e519387b2b765e891778642f2f28d17edf3a1
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2024-10-02 22:32:41 -0400

    Get Computer Choice
    Added method that returns random computer choice between rock, paper and scissor

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log(`Hello World`);
+let getComputerChoice = () => {
+    let randomComputerChoice = Math.floor(Math.random() * 3);
+    
+    if(randomComputerChoice === 0)
+        return "rock";
+    if(randomComputerChoice === 1)
+        return "paper";
+    if(randomComputerChoice === 2)
+        return "scissor";
+}
+
+console.log(getComputerChoice());
